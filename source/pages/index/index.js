9ca08548d4fc74dcc21cc7b4e429341d74ac6cde
Goshanbo <zhora.sem@yandex.ru>
2019-10-12 21:10:36 +0300

    заканчиваю с колонкой поллей ввода (text-field)

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,3 +1,5 @@
 import './index.scss';
 import 'popper.js';
 import 'bootstrap';
+import '../../Blocks/dropdown-with-math-field/dropdown-with-math-field.js';
+import '../../Blocks/text-field/text-field.js';
