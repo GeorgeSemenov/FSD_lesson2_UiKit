5c3195f460207c5471eca8cada9b64b0258fbcee
Goshanbo <zhora.sem@yandex.ru>
2019-12-11 20:03:00 +0300

    end up with curous behaviour of checkbox

--- source/Blocks/checkbox/checkbox.js
+++ source/Blocks/checkbox/checkbox.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+  var checkboxArr = $(".checkbox");
+  checkboxArr.each(function(){
+    if ($(this).children("input").prop('checked')){
+      $(this).toggleClass("checkbox_checked");
+    }
+  })
   $(".checkbox__inner-space").click(function(){
     console.log("hello");
     var checkbox = $(this).parent();
