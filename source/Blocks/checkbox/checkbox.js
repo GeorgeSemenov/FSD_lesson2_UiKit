7dab5f275f87914fbae766e3f65a5b27dd620562
Goshanbo <zhora.sem@yandex.ru>
2019-12-17 18:52:20 +0300

    for now i can affect on checkbox from the code

--- source/Blocks/checkbox/checkbox.js
+++ source/Blocks/checkbox/checkbox.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
   var checkboxArr = $(".checkbox");
   checkboxArr.each(function(){
-    if ($(this).children("input").prop('checked')){
-      $(this).toggleClass("checkbox_checked");
+    if($(this).hasClass("checkbox_checked")){
+      $(this).children("input").prop('checked', true)
     }else{
-      $(this).toggleClass("checkbox_unchecked");
+      $(this).children("input").prop('checked', false)
     }
+    // if ($(this).children("input").prop('checked')){
+    //   $(this).toggleClass("checkbox_checked");
+    // }else{
+    //   $(this).toggleClass("checkbox_unchecked");
+    // }
   })
   $(".checkbox__inner-space").click(function(){
     console.log("hello");
