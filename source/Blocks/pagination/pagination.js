49678008e19f050ead91aa071372f65e8ad63a48
GeorgeSemenov <zhora.sem@yandex.ru>
2020-03-20 15:20:07 +0300

    continue working on pagination feature

--- source/Blocks/pagination/pagination.js
+++ source/Blocks/pagination/pagination.js
@@ -1,7 +1,28 @@
 import '../../JScomponents/simplePagination/simplePagination.js';
+var pagination = $('.pagination__content');
+/*Функция призыва пагинации*/
 $(function(){
-  $('.pagination__content').pagination({
+  pagination.pagination({
     pages: 20,
-    cssStyle: 'light-theme'
+    cssStyle: 'light-theme',
+    displayedPages: 3,
+    edges: 1,
+    onPageClick:  function(pageNumber) {
+      // console.log("page number =" + pageNumber)
+    } 
   })
 })
+
+$(document).ready(function(){
+  $('.page-link')
+  .click(function(){
+    let href = this.attributes[0].value;
+    console.log(href);
+    console.log(href.length);
+    console.log(href[href.length - 1]);
+  })
+
+})
+
+
+
