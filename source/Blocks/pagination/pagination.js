01e502bfe2a0aa4c61c9f083e88c11c669d8cdbc
GeorgeSemenov <zhora.sem@yandex.ru>
2020-03-23 11:15:31 +0300

    some changes in pagination block

--- source/Blocks/pagination/pagination.js
+++ source/Blocks/pagination/pagination.js
@@ -2,6 +2,7 @@ import '../../JScomponents/simplePagination/simplePagination.js';
 var pagination = $('.pagination__content');
 /*Функция призыва пагинации*/
 $(function(){
+  // pagination.parent().append("<span>")
   pagination.pagination({
     pages: 20,
     cssStyle: 'light-theme',
@@ -9,20 +10,27 @@ $(function(){
     edges: 1,
     onPageClick:  function(pageNumber) {
       // console.log("page number =" + pageNumber)
+      if (pageNumber == 1){
+        console.log(`this = ${JSON.stringify(this)}`);
+        console.log(`more tha four`);
+        this["prevText"]= '';
+      }else{
+        this["prevText"]= 'Prev';
+      }
     } 
   })
 })
 
-$(document).ready(function(){
-  $('.page-link')
-  .click(function(){
-    let href = this.attributes[0].value;
-    console.log(href);
-    console.log(href.length);
-    console.log(href[href.length - 1]);
-  })
+// $(document).ready(function(){
+//   $('.page-link')
+//   .click(function(){
+//     let href = this.attributes[0].value;
+//     // console.log(href);
+//     // console.log(href.length);
+//     console.log(href[href.length - 1]);
+//   })
 
-})
+// })
 
 
 
