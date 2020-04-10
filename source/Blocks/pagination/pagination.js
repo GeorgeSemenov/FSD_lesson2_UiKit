d1645e03f1b3d8a5f2e01e3f6212a3e23402a1b1
GeorgeSemenov <zhora.sem@yandex.ru>
2020-04-10 14:58:03 +0300

    try to defead pagination, but holpless

--- source/Blocks/pagination/pagination.js
+++ source/Blocks/pagination/pagination.js
@@ -1,38 +1,14 @@
-import '../../JScomponents/simplePagination/simplePagination.js';
-var pagination = $('.pagination__content');
-/*Функция призыва пагинации*/
-$(function(){
-  // pagination.children().css({"border":"solid 10px red"});
-  console.log("it's working");
-  pagination.pagination({
-    pages: 20,
-    cssStyle: 'light-theme',
-    displayedPages: 3,
-    edges: 1,
-    onPageClick:  function(pageNumber) {
-      // console.log("page number =" + pageNumber)
-      if (pageNumber == 1){
-        console.log(`this = ${JSON.stringify(this)}`);
-        console.log(`more tha four`);
-        this["prevText"]= '';
-      }else{
-        this["prevText"]= 'Prev';
-      }
-    } 
-  })
-})
-
-$(document).ready(function(){
-  $('.page-link')
-  .click(function(){
-    console.log('Page-link is pressed');
-    let href = this.attributes[0].value;
-    // console.log(href);
-    // console.log(href.length);
-    console.log(href[href.length - 1]);
-  })
-
-})
-
-
-
+window.onload = function(){
+  var paginationPage = parseInt($('.cdp').attr('actpage'), 10);
+  $('.cdp_i').on('click', function(){
+    var go = $(this).attr('href').replace('#!', '');
+    if (go === '+1') {
+      paginationPage++;
+    } else if (go === '-1') {
+      paginationPage--;
+    }else{
+      paginationPage = parseInt(go, 10);
+    }
+    $('.cdp').attr('actpage', paginationPage);
+  });
+};
