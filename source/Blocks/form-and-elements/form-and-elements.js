5818edd36f3c3f5900296fd0d90be67811cf4949
Goshanbo <zhora.sem@yandex.ru>
2020-04-17 11:20:23 +0300

    ended up with like-button

--- source/Blocks/form-and-elements/form-and-elements.js
+++ source/Blocks/form-and-elements/form-and-elements.js
@@ -3,3 +3,4 @@ import '../text-field/text-field.js';
 import '../checkbox/checkbox.js';
 import '../range-slider/range-slider.js';
 import '../pagination/pagination.js';
+import '../like-button/like-button.js';
--- source/Blocks/like-button/like-button.js
+++ source/Blocks/like-button/like-button.js
@@ -1,1 +1,35 @@
+window.onload = function(){
+  $('.like-button').children('.like-button__wrapper')
+                   .children('.like-button__count').text()
+  /*let's check if there need to expand block*/
 
+  /*increasing and decreasing text value and toggle class _pressed*/
+  $('.like-button').on('click', function(){
+    var likeButton__count = $(this).children('.like-button__wrapper')
+           .children('.like-button__count');
+    var value = likeButton__count.text();
+    if ($(this).hasClass('like-button_pressed')){
+      value = +value - 1;
+      $(this).removeClass('like-button_pressed');
+      if($(this).hasClass('like-button_thousands-of-them') && (value < 1000)){
+        $(this).removeClass('like-button_thousands-of-them');
+        $(this).addClass('like-button_hundreds-of-them');
+      }else if($(this).hasClass('like-button_hundreds-of-them') && (value < 100)){
+        $(this).removeClass('like-button_hundreds-of-them');
+      }
+    } else{
+      value = +value + 1;
+      $(this).addClass('like-button_pressed');
+      if(!($(this).hasClass('like-button_hundreds-of-them')) && (value > 99)){
+        $(this).addClass('like-button_hundreds-of-them');
+      }else if(!($(this).hasClass('like-button_thousands-of-them')) && (value > 999)){
+        $(this).removeClass('like-button_hundreds-of-them');
+        $(this).addClass('like-button_hundreds-of-them');
+      }
+    }
+    value = $(this).hasClass('like-button_pressed') ?  (+value - 1) : (+value + 1);
+    $(this).toggleClass('like-button_pressed');
+
+    likeButton__count.text(value);
+  })
+}
