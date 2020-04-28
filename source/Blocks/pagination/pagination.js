b308a6522474ae43720cac9bee03110d4349daa8
Goshanbo <zhora.sem@yandex.ru>
2020-04-28 22:17:30 +0300

    end up with process of pagination, start working on pagination pixel perfect

--- source/Blocks/pagination/pagination.js
+++ source/Blocks/pagination/pagination.js
@@ -1,7 +1,10 @@
  $(window).on('load', function () {
+  /*функционал необходимый для работы самой пагинации, тут изменяется значение actpage*/
   var paginationPage = parseInt($('.pagination__content').attr('actpage'), 10);
   $('.pagination__pagination-item').on('click', function(){
     var go = $(this).attr('href').replace('#!', '');
+    var valueOfCalculation = $(this).parent().parent().children('.pagination__display-calculations').children('i');
+    console.log(valueOfCalculation.text());
     if (go === '+1') {
       paginationPage++;
     } else if (go === '-1') {
