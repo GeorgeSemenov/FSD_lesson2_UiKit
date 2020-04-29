cfec55fa45b92050961e9e85d53f2a388ca6e77d
GeorgeSemenov <zhora.sem@yandex.ru>
2020-04-29 11:27:38 +0300

    tring to make pagination PP, not successeful yet

--- source/Blocks/pagination/pagination.js
+++ source/Blocks/pagination/pagination.js
@@ -3,8 +3,6 @@
   var paginationPage = parseInt($('.pagination__content').attr('actpage'), 10);
   $('.pagination__pagination-item').on('click', function(){
     var go = $(this).attr('href').replace('#!', '');
-    var valueOfCalculation = $(this).parent().parent().children('.pagination__display-calculations').children('i');
-    console.log(valueOfCalculation.text());
     if (go === '+1') {
       paginationPage++;
     } else if (go === '-1') {
@@ -13,5 +11,11 @@
       paginationPage = parseInt(go, 10);
     }
     $('.pagination__content').attr('actpage', paginationPage);
+
+    /*тут происходит изменение количества вариантов аренды */
+    var valueOfCalculation = $(this).parent().parent()
+                                    .children('.pagination__display-calculations')
+                                    .children('i');
+    valueOfCalculation.text(`${(paginationPage-1)*12 + 1} - ${paginationPage*12}`)
   });
 });
