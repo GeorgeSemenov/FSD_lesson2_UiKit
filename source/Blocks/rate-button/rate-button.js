d7ef1af9e60988e33055283315a7e0ff73b92155
GeorgeSemenov <zhora.sem@yandex.ru>
2020-06-01 14:49:26 +0300

    rate-button stars added

--- source/Blocks/rate-button/rate-button.js
+++ source/Blocks/rate-button/rate-button.js
@@ -1,1 +1,20 @@
+$(document).ready(function(){
+  //Закрашивание нужного количества звёздочек при первой загрузке страницы
+  $(".rate-button").each(function(index,resulItem){
+    $(this).data('')
+  })
 
+  //Изменение значений окрашенных звёздочек при клике
+  $(".rate-button__star").click(function(){
+    let stars = $(this).parent().children(".rate-button__star");
+    let nops = $(this).data('number');// means number of pressed star
+    stars.each(function(index, resultItem){
+      $(this).removeClass('.rate-button__star_filed');
+      $(this).removeAttr("fill");
+      if (index < nops){
+        $(this).addClass('rate-button__star_filed');
+        $(this).attr("fill", "url(#gradt)");
+      }
+    })
+  })
+})
