817333175a27ed98095eaf3212494a7e5a3c8505
GeorgeSemenov <zhora.sem@yandex.ru>
2020-07-14 11:52:49 +0300

    delete all bloack cause i have to make first letter of folder Blocks is smaller, like blocks

--- source/Blocks/checkbox-list/checkbox-list.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(".checkbox-list__arrow-toggler")
-    .click(function(){
-      $(this).parent().parent(".checkbox-list")
-             .toggleClass("checkbox-list_expanded");
-
-    })
-})
